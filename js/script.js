621ecacba5e22615f844e74c3b870f885873930e
Alana Nanz <97478674+alana-nanz@users.noreply.github.com>
2022-02-18 16:46:06 -0500

    Added JS styles for clicking the arrow and expanding the section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 const question = document.querySelector(".question");
 const questionText = document.querySelector("h3");
 const answer = document.querySelector(".answer");
+const arrow = document.querySelector(".arrow");
 
 question.addEventListener("click", function () {
-    answer.classList.remove("hide");
-    questionText.style.fontWeight = 700;
+    if (answer.classList.contains("hide")) {
+        answer.classList.remove("hide");
+        questionText.style.fontWeight = 700;
+        arrow.style.transform = "rotate(180deg)";
+    } else {
+        answer.classList.add("hide");
+        questionText.style.fontWeight=400;
+        arrow.style.transform = "rotate(0deg)";
+    };
 });
-
